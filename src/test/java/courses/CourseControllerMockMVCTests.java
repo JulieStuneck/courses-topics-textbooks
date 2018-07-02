4445844d14d056d9df3dcf93750dc6155d5d0451
JulieStuneck <jbstuneck@yahoo.com>
2018-07-01 20:52:53 -0400

    through video

--- src/test/java/courses/CourseControllerMockMVCTests.java
+++ src/test/java/courses/CourseControllerMockMVCTests.java
@@ -3,6 +3,8 @@ package courses;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 
 import javax.annotation.Resource;
@@ -31,8 +33,20 @@ public class CourseControllerMockMVCTests {
 	@MockBean
 	private TopicRepository topicRepo;
 	
+	@MockBean
+	private TextbookRepository textbookRepo;
+	
 	@Mock
 	private Course course;
+	
+	@Mock
+	private Course anotherCourse;
+	
+	@Mock
+	private Topic topic;
+	
+	@Mock
+	private Topic anotherTopic;
 	
 	@Test
 	public void shouldRouteToSingleCourseView() throws Exception {
@@ -40,5 +54,87 @@ public class CourseControllerMockMVCTests {
 		when(courseRepo.findById(arbitraryCourseId)).thenReturn(Optional.of(course));
 		mvc.perform(get("/course?id=1")).andExpect(view().name(is("course")));
 	}
+	
+	@Test
+	public void shouldBeOkForSingleCourse() throws Exception {
+		long arbitraryCourseId = 1;
+		when(courseRepo.findById(arbitraryCourseId)).thenReturn(Optional.of(course));
+		mvc.perform(get("/course?id=1")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldNotBeOkForSingleCourse() throws Exception {
+		mvc.perform(get("/course?id=1")).andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void shouldPutSingleCourseIntoModel() throws Exception {
+		when(courseRepo.findById(1L)).thenReturn(Optional.of(course));
+		
+		mvc.perform(get("/course?id=1")).andExpect(model().attribute("courses", is(course)));
+	}
+	
+	@Test
+	public void shouldRouteToAllCoursesView() throws Exception {
+		mvc.perform(get("/show-courses?id=1")).andExpect(view().name(is("courses")));
+	}
+	
+	@Test
+	public void shouldBeOkForAllCourses() throws Exception {
+		mvc.perform(get("/show-courses")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldPutAllCoursesIntoModel() throws Exception {
+		Collection<Course> allCourses = Arrays.asList(course, anotherCourse);
+		when(courseRepo.findAll()).thenReturn(allCourses);
+		
+		mvc.perform(get("/show-courses")).andExpect(model().attribute("courses", is(allCourses)));
+	}
+	
+	@Test
+		public void shouldRouteToSingleTopicView() throws Exception {
+		long arbitraryTopicId = 1;
+		when(topicRepo.findById(arbitraryTopicId)).thenReturn(Optional.of(topic));
+		mvc.perform(get("/topic?id=1")).andExpect(view().name(is("topic")));
+	}
+	
+	@Test
+	public void shouldBeOkForSingleTopic() throws Exception {
+		long arbitraryTopicId = 1;
+		when(topicRepo.findById(arbitraryTopicId)).thenReturn(Optional.of(topic));
+		mvc.perform(get("/topic?id=1")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldNotBeOkForSingleTopic() throws Exception {
+		mvc.perform(get("/topic?id=1")).andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void shouldPutSingleTopicIntoModel() throws Exception {
+		when(topicRepo.findById(1L)).thenReturn(Optional.of(topic));
+		
+		mvc.perform(get("/topic?id=1")).andExpect(model().attribute("topics", is(topic)));
+	}
+	
+	@Test
+	public void shouldRouteToAllTopicsView() throws Exception {
+		mvc.perform(get("/show-topics?id=1")).andExpect(view().name(is("topics")));
+	}
+	
+	@Test
+	public void shouldBeOkForAllTopics() throws Exception {
+		mvc.perform(get("/show-topics")).andExpect(status().isOk());
+	}
+	
+	@Test
+	public void shouldPutAllTopicsIntoModel() throws Exception {
+		Collection<Topic> allTopics = Arrays.asList(topic, anotherTopic);
+		when(topicRepo.findAll()).thenReturn(allTopics);
+		
+		mvc.perform(get("/show-topics")).andExpect(model().attribute("topics", is(allTopics)));
+	}
+	
 
 }
